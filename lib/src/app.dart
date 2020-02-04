1523d1707989a05a9c55af4fbc319b9dbb006bb7
Ben Force <benforce@gmail.com>
2020-02-04 08:55:43 -0600

    feat: add test filtering

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -18,38 +18,84 @@ class TestBedApp extends StatefulWidget {
 }
 
 class _TestBedAppState extends State<TestBedApp> {
+  TextEditingController searchQueryController = TextEditingController();
+  String filter = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    searchQueryController.addListener(() =>
+        setState(() => filter = searchQueryController.text.toLowerCase()));
+  }
+
   @override
   Widget build(BuildContext context) {
     List<WidgetTest> tests = widget.tests;
+    ThemeData theme = Theme.of(context);
+    TextTheme primaryTextTheme = theme.primaryTextTheme;
 
     return MaterialApp(
       title: 'Flutter Testbed',
       theme: widget.theme,
       home: BlocProvider<ShellBloc>(
-          builder: (BuildContext context) =>
+          create: (BuildContext context) =>
               widget.bloc..add(ShellReloadStateEvent()),
           child: Scaffold(
             appBar: AppBar(title: Text("Flutter Testbed")),
-            drawer: Drawer(
-                child: ListView(
-              children: <Widget>[
-                DrawerHeader(
-                  child: Text(
-                    "Widgets",
-                    style: Theme.of(context).primaryTextTheme.headline6,
-                  ),
-                  decoration:
-                      BoxDecoration(color: Theme.of(context).primaryColor),
-                ),
-                ...tests
-                    .map((test) => ListTile(
-                        title: Text(test.label),
-                        onTap: () {
-                          widget.bloc.add(SetTestShellEvent(test.label));
-                        }))
-                    .toList()
-              ],
-            )),
+            drawer: BlocBuilder<ShellBloc, ShellState>(
+              builder: (BuildContext context, state) {
+                var selectedLabel = "";
+                if (state is ShellTestSelected) {
+                  selectedLabel = state.testLabel;
+                }
+
+                return Drawer(
+                    child: Column(
+                  children: <Widget>[
+                    DrawerHeader(
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            "Widgets",
+                            style: primaryTextTheme.title,
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: "Search",
+                                suffixIcon: Icon(Icons.search)),
+                            controller: searchQueryController,
+                          )
+                        ],
+                      ),
+                      decoration:
+                          BoxDecoration(color: Theme.of(context).primaryColor),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                          itemCount: tests.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            var test = tests[index];
+
+                            if (filter != null &&
+                                filter.trim().length > 0 &&
+                                !test.label.toLowerCase().contains(filter)) {
+                              return Container();
+                            }
+                            return ListTile(
+                                title: Text(test.label),
+                                selected: test.label == selectedLabel,
+                                onTap: () {
+                                  Navigator.of(context).pop();
+                                  widget.bloc
+                                      .add(SetTestShellEvent(test.label));
+                                });
+                          }),
+                    )
+                  ],
+                ));
+              },
+            ),
             body: Padding(
               padding: EdgeInsets.all(12.0),
               child: BlocBuilder<ShellBloc, ShellState>(
@@ -65,14 +111,14 @@ class _TestBedAppState extends State<TestBedApp> {
                     return Center(
                         child: Text(
                       "Could not find test ${state.testLabel}",
-                      style: Theme.of(context).textTheme.subtitle2,
+                      style: theme.textTheme.display1,
                     ));
                   }
 
                   return Center(
                       child: Text(
                     "Select a test from the sidebar",
-                    style: Theme.of(context).textTheme.subtitle2,
+                    style: theme.textTheme.display1,
                   ));
                 },
               ),
